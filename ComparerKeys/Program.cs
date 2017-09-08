a81cc9ba1117ee27addc50ccb6cb87f4c2c114f3
Andrei Mungiu <andrei_mungiu@yahoo.com>
2017-09-08 20:31:08 +0200

    Tutorial checkpoint

--- ComparerKeys/Program.cs
+++ ComparerKeys/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace ComparerKeys
 {
@@ -7,17 +8,41 @@ namespace ComparerKeys
     {
         static void Main(string[] args)
         {
-            //supplying a parameter to the dictionary contructor
-            //and giving it an initialization list to set elements
-            var primeMinisters = new Dictionary<string, PrimeMinister>
-                (StringComparer.OrdinalIgnoreCase)
+            //previously this was a "Dictionary<string, PrimeMinister>"
+            //SortedList stores the list sorted by the key
+            //SortedDictionary stores the list sorted by "balancedtree" hierarchy
+            var primeMinisters = new SortedDictionary<string, PrimeMinister>
+                //supplying parameter to dictionary contructor
+                (new UncasedStringComparer())
+                //initialization list to set elements
             {
                 {"JC", new PrimeMinister("James Callaghan", 1974) },
                 {"MT", new PrimeMinister("Margaret Thatcher", 1979) },
                 {"TB", new PrimeMinister("Tony Blair", 1997) }
             };
-
+            primeMinisters.Add("AM", new PrimeMinister("Andrei Mungiu", 2017));
+            //due to additional parameter from above, case sensitivity is ignored
             Console.WriteLine(primeMinisters["tb"]);
+
+
+            ///creating read only Dictionary from the above Dictionary
+            var pmsReadOnly = new ReadOnlyDictionary
+                <string, PrimeMinister>(primeMinisters);
+            foreach (var pm in pmsReadOnly)
+            {
+                Console.WriteLine(pm);
+            }
+        }
+
+        //hash code is irrelevant -> sorted list doesn't have hash table
+        class UncasedStringComparer : IComparer<string>
+        {
+            public int Compare(string x, string y)
+            {
+                //Microsoft default string comparer with ignore case overload
+                return string.Compare
+                    (x, y, StringComparison.CurrentCultureIgnoreCase);
+            }
         }
     }
 }
